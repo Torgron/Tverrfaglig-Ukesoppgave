516c789d5bc8681d637049c9675da80d33b293a7
Tor Grønås <tor.gronas@hotmail.com>
2022-04-27 16:22:15 +0200

    test11

--- controller/app.js
+++ controller/app.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: '/../.env' });
+dotenv.config({ path: '../.env' });
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require( "passport-google-oauth2" ).Strategy;
