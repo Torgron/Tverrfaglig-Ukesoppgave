69f3a3f27266393e50f22212de6433c1016e93d1
Tor Grønås <tor.gronas@hotmail.com>
2022-04-28 12:07:18 +0200

    siste finpuss (kommentarer og responsive fix)

--- controller/app.js
+++ controller/app.js
@@ -1,3 +1,4 @@
+//Her bruker jeg node funksjonen require() for å laste inn nødvendige node moduler
 const dotenv = require("dotenv");
 dotenv.config({ path: '../.env' });
 const express = require("express");
@@ -5,21 +6,28 @@ const passport = require("passport");
 const GoogleStrategy = require( "passport-google-oauth2" ).Strategy;
 const favicon = require("serve-favicon");
 
+//Her lages en ny express applikasjon ved å tildele express() funksjonen til en variabel
 const app = express();
 
+//Her settes EJS som view engine for express applikasjonen
 app.set("view engine", "ejs");
 
+//Her blir det lagt til middleware for å gjøre det mulig å få tilgang til filer i denne mappen via HTTP
 app.set("views", "../views");
 app.use(express.static("views"));
 
+//Her blir det lastet inn et HTTP-rammeverk som brukes for å lagre brukerdata mellom HTTP-forespørsler
 const session = require("express-session");
 
+//Her brukes express.static() igjen for å kunne nå filene i de valgte mappene via den første stien
 app.use("/styles", express.static("../views/styles"));
 app.use("/css", express.static("../node_modules/bootstrap/dist/css"));
 app.use("/js", express.static("../node_modules/bootstrap/dist/js"));
 app.use("/js", express.static("../node_modules/jquery/dist"));
 app.use(favicon("../views/images/favicon.ico"));
 
+
+//Passport er autentiseringsmiddleware for Node.js. GoogleStrategy er en måte for Passport å autentisere med Google. 
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID || process.env.CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || process.env.CLIENT_SECRET,
@@ -30,6 +38,9 @@ passport.use(new GoogleStrategy({
         return done(null, profile);
     }
 ));
+
+
+//Her blir brukeren lagret og det blir bestemt hva slags brukerdata som skal bli lagret i økten
 app.use(session ({
     resave: false, 
     saveUninitialized: true, 
@@ -47,6 +58,8 @@ passport.deserializeUser(function(obj, cb) {
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+//Her sjekkes det om brukeren er logget inn, hvis ikke vil brukeren blir sendt til "/" routen
 function loggedIn(req, res, next) {
     if (req.user) {
         next();
@@ -55,6 +68,8 @@ function loggedIn(req, res, next) {
     }
 }
 
+
+//Her blir GET og POST forespørsler håndtert og bruker blir sendt til de forskjellige sidene samt det testes om brukeren er autentisert
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -94,6 +109,8 @@ app.post("/auth/google/success/security", (req, res) => {
     });
 });
 
+
+//Her er porten serveren kjører på
 
 const PORT = process.env.PORT || 8080;
 
